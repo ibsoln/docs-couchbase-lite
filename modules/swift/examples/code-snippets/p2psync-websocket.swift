b642b57347875b6cd192ea296f9ffcad4b336e2a
ibsoln <52778946+ibsoln@users.noreply.github.com>
2020-06-24 09:39:04 +0100

    h2Beta - Code samples Changes in prepararation of Swift iOS for Beta testing

--- modules/swift/examples/code-snippets/p2psync-websocket.swift
+++ modules/swift/examples/code-snippets/p2psync-websocket.swift
@@ -55,7 +55,8 @@ class URLEndpontListenerTest: ReplicatorTest {
     //  ... Couchbase Lite Database initialization that returns `_userDB`
 
     guard let db = _userDb else {
-        throw ListDocError.DatabaseNotInitialized
+      throw print("DatabaseNotInitialized")
+      // ... take appropriate actions
     }
     var listener: URLEndpointListener?
     let config = URLEndpointListenerConfiguration.init(database: db)
@@ -502,12 +503,12 @@ _websocketListener = URLEndpointListener(config: listenerConfig)
     func startP2PReplicationWithUserDatabaseToRemotePeer(_ peer:PeerHost, handler:@escaping(_ status:PeerConnectionStatus)->Void) throws{
         print("\(#function) with ws://\(peer)/\(kUserDBName)")
         guard let userDb = _userDb else {
-             throw ListDocError.DatabaseNotInitialized
-
+          throw print("DatabaseNotInitialized")
+          // ... take appropriate actions
         }
         guard let user = self.currentUserCredentials?.user, let password = self.currentUserCredentials?.password else {
-                    throw ListDocError.UserCredentialsNotProvided
-
+          throw print("UserCredentialsNotProvided")
+          // ... take appropriate actions
         }
 
 // tag::replicator-start-func-config-init[]
@@ -516,7 +517,8 @@ _websocketListener = URLEndpointListener(config: listenerConfig)
         if replicatorForUserDb == nil {
             // Start replicator to connect to the URLListenerEndpoint
             guard let targetUrl = URL(string: "ws://\(peer)/\(kUserDBName)") else {
-                throw ListDocError.URLInvalid
+                throw print("URLInvalid")
+                // ... take appropriate actions
             }
 
 
@@ -636,8 +638,9 @@ class myclass {
         _websocketListener = URLEndpointListener(config: listenerConfig) // <5>
 
         guard let websocketListener = _websocketListener else {
-            throw ListDocError.WebsocketsListenerNotInitialized
-            }
+          throw print("WebsocketsListenerNotInitialized")
+          // ... take appropriate actions
+        }
         try websocketListener.start() // <6> <7>
 
 // end::listener-initialize[]
@@ -671,7 +674,8 @@ listenerConfig.enableDeltaSync = true
 // tag::listener-start[]
 _websocketListener = URLEndpointListener(config: listenerConfig)
 guard let websocketListener = _websocketListener else {
-    throw ListDocError.WebsocketsListenerNotInitialized
+  throw print("WebsocketsListenerNotInitialized")
+  // ... take appropriate actions
     }
 try websocketListener.start()
 // end::listener-start[]
@@ -736,54 +740,278 @@ self.listener = URLEndpointListener.init(config: config) // <1>
 
 // Active Peer Connection Snippets
 
-// tag::p2p-act-rep-func[]
+//
+//  my Other Bits.swift
+//  doco-sync
+//
+//  Created by Ian Bridge on 19/06/2020.
+//  Copyright © 2020 Couchbase Inc. All rights reserved.
+//
 
-Let user = //
-Let password = //
-Let cert:SecCertificate = //
-Let passivePeerEndpoint = //
-Let passivePeerPort = //
-Let passivePeerDb = //
+import Foundation
+import CouchbaseLiteSwift
+import MultipeerConnectivity
 
-// tag::p2p-act-rep-initialize[]
-    var database: Database?
-    var replicator: Replicator? // <1>
+class myActPeerClass {
 
-    let url = URL(string: "ws://"+ passivePeerEndpoint + ":" + passivePeerPort +"/" + passivePeerDb)!
-    // <2> <3>
-    let targetEndpoint = URLEndpoint(url: url)
+  func fMyActPeer() {
+    // tag::p2p-act-rep-func[]
+    let user = "syncuser"
+    let password = "sync9455"
+    let cert:SecCertificate?
+    let passivePeerEndpoint = "10.1.1.12:8920"
+    let passivePeerPort = "8920"
+    let passiveDbName = "userdb"
+    var actDb:Database?
+    var thisReplicator:Replicator?
+    var replicatorListener:ListenerToken?
 
-    let config = ReplicatorConfiguration(database: database!, target: targetEndpoint)  // <4>
-// end::p2p-act-rep-initialize[]
 
-// tag::p2p-act-rep-config[]
-    config.replicatorType = .pushandpull
+    // tag::p2p-act-rep-initialize[]
+    let tgtUrl = URL(string: "wss://10.1.1.12:8092/actDb")!
+    let targetEndpoint = URLEndpoint(url: tgtUrl)
+    var config = ReplicatorConfiguration(database: actDb!, target: targetEndpoint) // <1>
+    // end::p2p-act-rep-initialize[]
+
+    // tag::p2p-act-rep-config[]
+    // tag::p2p-act-rep-config-type[]
+    config.replicatorType = .pushAndPull
+    // end::p2p-act-rep-config-type[]
+    // tag::p2p-act-rep-config-cont[]
     config.continuous = true
-    config.serverCertificateVerificationMode = .selfSignedCert // <5>
-// end::p2p-act-rep-config[]
+    // end::p2p-act-rep-config-cont[]
+    // tag::p2p-act-rep-config-cert-verify[]
+    // serverCertificateVerificationMode=.selfSignedCert effectively disables cert validation
+    config.serverCertificateVerificationMode = .selfSignedCert // <2>
+    // end::p2p-act-rep-config-cert-verify[]
+    // end::p2p-act-rep-config[]
 
-// tag::p2p-act-rep-auth[]
-//  Set Authentication Mode
-    let authenticator = BasicAuthenticator(username: user, password: password)
+    // tag::p2p-act-rep-auth[]
+    //  Set Authentication Mode
+    let authenticator = BasicAuthenticator(username: user, password: password) // <3>
     config.authenticator = authenticator
+    // end::p2p-act-rep-auth[]
 
-    replicator = Replicator.init( config: config)
-// end::tag::p2p-act-rep-auth[]
+    // tag::p2p-act-rep-start-full[]
+    // Apply configuration settings to the replicator
+    thisReplicator = Replicator.init( config: config) // <4>
+
+    // Optionally add a change listener
+    // tag::p2p-act-rep-add-change-listener[]
+    // add change listener and retain token for use in deletion
+    let pushPullReplListener:ListenerToken? = thisReplicator?.addChangeListener({ (change) in // <5>
+      if change.status.activity == .stopped {
+          print("Replication stopped")
+      }
+      else {
+      // tag::p2p-act-rep-status[]
+          print("Replicator is currently ", thisReplicator?.status.activity)
+      }
+    })
+    // end::p2p-act-rep-status[]
+// end::p2p-act-rep-add-change-listener[]
 
 // tag::p2p-act-rep-start[]
-    replicator?.start()
+    // Run the replicator using the config settings
+    thisReplicator?.start()  // <6>
 // end::p2p-act-rep-start[]
-
-// tag::p2p-act-rep-add-change-listener[]
-    let pushPullListener = replicator?.addChangeListener { (change) in
-        if change.status.activity == .stopped {
-            print("Replication stopped")
-        }
-    }
-// tag::p2p-act-rep-status[]
-    print("Replicator is current;y ", replicator?.status.activity)
-// end::p2p-act-rep-status[]
-// end::p2p-act-rep-add-change-listener[]
+// end::p2p-act-rep-start-full[]
 
 
 // end::p2p-act-rep-func[]
+    }
+
+    func mystopfunc() {
+// tag::p2p-act-rep-stop[]
+    // Remove the change listener
+
+    thisReplicator?.removeChangeListener(withToken: pushPullReplListener)
+    // Stop the replicator
+    thisReplicator?.stop()
+
+// end::p2p-act-rep-stop[]
+}
+
+
+
+
+
+
+
+// tag::p2p-tlsid-manage-func[]
+//
+//  cMyGetCert.swift
+//  doco-sync
+//
+//  Created by Ian Bridge on 20/06/2020.
+//  Copyright © 2020 Couchbase Inc. All rights reserved.
+//
+
+import Foundation
+import Foundation
+import CouchbaseLiteSwift
+import MultipeerConnectivity
+
+
+class cMyGetCert1{
+
+    let kListenerCertLabel = "doco-sync-server"
+    let kListenerCertKeyP12File = "listener-cert-pkey"
+    let kListenerPinnedCertFile = "listener-pinned-cert"
+    let kListenerCertKeyExportPassword = "couchbase"
+    //var importedItems : NSArray
+    let thisData : CFData?
+    var items : CFArray?
+
+    func fMyGetCert() ->TLSIdentity? {
+        var kcStatus = errSecSuccess // Zero
+        let thisLabel : String? = "doco-sync-server"
+
+        //var thisData : CFData?
+        // tag::p2p-tlsid-check-keychain[]
+        // tag::p2p-tlsid-tlsidentity-with-label[]
+        // USE KEYCHAIN IDENTITY IF EXISTS
+        // Check if Id exists in keychain. If so use that Id
+        do {
+            if let thisIdentity = try TLSIdentity.identity(withLabel: "doco-sync-server") {
+                print("An identity with label : doco-sync-server already exists in keychain")
+                return thisIdentity
+                }
+        } catch
+          {return nil}
+// end::p2p-tlsid-check-keychain[]
+        thisAuthenticator.ClientCertificateAuthenticator(identity: thisIdentity )
+        config.thisAuthenticator
+// end::p2p-tlsid-tlsidentity-with-label[]
+
+
+// tag::p2p-tlsid-check-bundled[]
+// CREATE IDENTITY FROM BUNDLED RESOURCE IF FOUND
+
+        // Check for a resource bundle with required label to generate identity from
+        // return nil identify if not found
+        guard let pathToCert = Bundle.main.path(forResource: "doco-sync-server", ofType: "p12"),
+                let thisData = NSData(contentsOfFile: pathToCert)
+            else
+                {return nil}
+// end::p2p-tlsid-check-bundled[]
+
+// tag::p2p-tlsid-import-from-bundled[]
+        // Use SecPKCS12Import to import the contents (identities and certificates)
+        // of the required resource bundle (PKCS #12 formatted blob).
+        //
+        // Set passphrase using kSecImportExportPassphrase.
+        // This passphrase should correspond to what was specified when .p12 file was created
+        kcStatus = SecPKCS12Import(thisData as CFData, [String(kSecImportExportPassphrase): "couchbase"] as CFDictionary, &items)
+            if kcStatus != errSecSuccess {
+             print("failed to import data from provided with error :\(kcStatus) ")
+             return nil
+            }
+        let importedItems = items! as NSArray
+        let thisItem = importedItems[0] as! [String: Any]
+
+        // Get SecIdentityRef representing the item's id
+        let thisSecId = thisItem[String(kSecImportItemIdentity)]  as! SecIdentity
+
+        // Get Id's Private Key, return nil id if fails
+        var thisPrivateKey : SecKey?
+        kcStatus = SecIdentityCopyPrivateKey(thisSecId, &thisPrivateKey)
+            if kcStatus != errSecSuccess {
+                print("failed to import private key from provided with error :\(kcStatus) ")
+                return nil
+            }
+
+        // Get all relevant certs [SecCertificate] from the ID's cert chain using kSecImportItemCertChain
+        let thisCertChain = thisItem[String(kSecImportItemCertChain)] as? [SecCertificate]
+
+        // Return nil Id if errors in key or cert chain at this stage
+        guard let pKey = thisPrivateKey, let pubCerts = thisCertChain else {
+            return nil
+        }
+// end::p2p-tlsid-import-from-bundled[]
+
+// tag::p2p-tlsid-store-in-keychain[]
+// STORE THE IDENTITY AND ITS CERT CHAIN IN THE KEYCHAIN
+
+        // Store Private Key in Keychain
+        let params: [String : Any] = [
+            String(kSecClass):          kSecClassKey,
+            String(kSecAttrKeyType):    kSecAttrKeyTypeRSA,
+            String(kSecAttrKeyClass):   kSecAttrKeyClassPrivate,
+            String(kSecValueRef):       pKey
+        ]
+        kcStatus = SecItemAdd(params as CFDictionary, nil)
+            if kcStatus != errSecSuccess {
+                print("Unable to store private key")
+                return nil
+            }
+       // Store all Certs for Id in Keychain:
+       var i = 0;
+       for cert in thisCertChain! {
+            let params: [String : Any] = [
+                String(kSecClass):      kSecClassCertificate,
+                String(kSecValueRef):   cert,
+                String(kSecAttrLabel):  "doco-sync-server"
+                ]
+            kcStatus = SecItemAdd(params as CFDictionary, nil)
+                if kcStatus != errSecSuccess {
+                    print("Unable to store certs")
+                    return nil
+                }
+            i=i+1
+        }
+// end::p2p-tlsid-store-in-keychain[]
+
+// tag::p2p-tlsid-return-id-from-keychain[]
+// RETURN A TLSIDENTITY FROM THE KEYCHAIN FOR USE IN CONFIGURING TLS COMMUNICATION
+do {
+    return try TLSIdentity.identity(withIdentity: thisSecId, certs: [pubCerts[0]])
+} catch {
+    print("Error while loading self signed cert : \(error)")
+    return nil
+}
+// end::p2p-tlsid-return-id-from-keychain[]
+    } // fMyGetCert
+} // cMyGetCert
+
+// end::p2p-tlsid-manage-func[]
+
+
+
+
+
+
+
+// tag::p2p-act-rep-config-self-cert[]
+// Use serverCertificateVerificationMode set to `.selfSignedCert` to disable cert validation
+config.disableTLS = false
+config.serverCertificateVerificationMode = .selfSignedCert
+// end::p2p-act-rep-config-self-cert[]
+
+
+// tag::p2p-act-rep-config-cacert-pinned-func[]
+func fMyCaCertPinned() {
+  // do {
+  let tgtUrl = URL(string: "wss://10.1.1.12:8092/actDb")!
+  let targetEndpoint = URLEndpoint(url: tgtUrl)
+  let actDb:Database?
+  let config = ReplicatorConfiguration(database: actDb!, target: targetEndpoint)
+  // tag::p2p-act-rep-config-cacert-pinned[]
+
+  // Get bundled resource and read into localcert
+  guard let pathToCert = Bundle.main.path(forResource: "listener-pinned-cert", ofType: "cer")
+    else { /* process error */ }
+  guard let localCertificate:NSData = NSData(contentsOfFile: pathToCert!)
+    else { /* process error */ }
+
+  // Create certificate
+  // using its DER representation as a CFData
+  guard let pinnedCert = SecCertificateCreateWithData(nil, localCertificate)
+    else { /* process error */ }
+
+  // Add `pinnedCert` and `.cacert` to `ReplicatorConfiguration`
+  config.serverCertificateVerificationMode = .caCert
+  config.pinnedServerCertificate = pinnedCert
+  // end::p2p-act-rep-config-cacert-pinned[]
+  // end::p2p-act-rep-config-cacert-pinned-func[]
+}
